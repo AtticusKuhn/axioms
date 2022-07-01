697406343548847f44214d598daeef1a8d0a54b6
eulerthedestroyer <eulerthedestroyer@gmail.com>
2022-07-01 13:31:43 -0400

    help me

--- src/uft.lean
+++ src/uft.lean
@@ -1,4 +1,5 @@
 import .ring
+import .set_1
 -- import tactic
 open ring
 variables {R : Type} [ring R]
@@ -10,7 +11,8 @@ theorem WOP_Contradiction:
   ( ∀(minimal: ZZ), ∃(smaller: ZZ),
   ¬proposition(minimal) → ¬proposition(smaller) 
   ∧ smaller < minimal ) → (∀ (x : ZZ), 
-  proposition(x)) := begin
+  proposition(x)) 
+  := begin
   intros prop holds_for_smaller all,
   by_contradiction,
   -- let notprop: ∀(x:ZZ), x → Prop := ¬ proposition(x),
@@ -31,6 +33,37 @@ theorem WOP_Contradiction:
   sorry,
 end 
 
+theorem Euclidean_Algorithm_Exists: ∀ (a b: ZZ), ∃ (q r :ZZ), a=b*q+r := begin
+intros a b,
+split, {
+  split, {
+    have : a = b*0 + a := begin
+    rw mul_zero,
+    rw zero_add,
+    end,
+    exact this,
+  },
+},
+end
+
+theorem Contra_Euclidean_Algorithm_One_Step:∃ (a b: ZZ), ¬ ( ∀ (q r :ZZ), (a=b*q+r)→  r > b) := begin
+let euclidean : ZZ → Prop := λ a, ∀ (b:ZZ), ¬∀ (  q r : ZZ), a = b * q + r →  r > b,
+have := WOP_Contradiction (euclidean) ,
+have thing :(∀ (minimal : ZZ), ∃ (smaller : ZZ), ¬euclidean minimal → ¬euclidean smaller ∧ smaller < minimal)  := begin
+intros minimal,
+split, {
+  intros em,
+  change (¬euclidean minimal) with (∀ (a b q r :ZZ), (a=b*q+r)→  r > b) at  em,
+  -- rw euclidean at em,
+sorry,
+},
+sorry,
+end,
+ have that := this thing,
+exact that,
+
+end
+
 --  def
 theorem Euclidean_Algorithm_One_Step: ∀ (a b: ZZ), ∃ (q r :ZZ), a=b*q+r ∧ r < b := begin
 -- intros a b,
@@ -39,6 +72,8 @@ have := WOP_Contradiction (euclidean) ,
 have thing : (∀ (minimal : ZZ), ∃ (smaller : ZZ), ¬euclidean minimal → ¬euclidean smaller ∧ smaller < minimal)  := begin 
 intros a,
 split, {
+intros na,
+
 -- rw euclidean,
 sorry,
 },
